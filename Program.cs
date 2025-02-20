ed587218b4f38c6e8770af9a138509937a967a9f
Chloe Vandeworp <339639510@GoTVDSB.ca>
2025-02-20 13:32:03 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_4._5___Intro_to_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
